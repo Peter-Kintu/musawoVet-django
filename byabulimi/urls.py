ae8025809427d50284018c2744a74ccd3a8da9a0
Kintu Peter <kintupeter721@gmail.com>
2025-12-15 18:31:11 +0300

    v2

--- byabulimi/urls.py
+++ byabulimi/urls.py
@@ -1,7 +1,7 @@
 # byabulimi/urls.py
 
 from django.urls import path
-from .migrations import views
+from . import views
 from rest_framework.authtoken.views import obtain_auth_token
 
 # Define URL patterns for the API
